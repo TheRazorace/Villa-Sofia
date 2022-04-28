ef8d3d68eaafe2bfbe2b7ab30af64c366301694d
TheRazorace <49875599+TheRazorace@users.noreply.github.com>
2022-04-28 03:21:12 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -52,4 +52,86 @@ var abswiper = new Swiper(".aboutSwiper", {
             delay: 5700,
             disableOnInteraction: false
         },
-      });
+});
+
+let room_item = document.querySelector(".room-item");
+
+let a1 = document.querySelector("#a1");
+let a2 = document.querySelector("#a2");
+let a3 = document.querySelector("#a3");
+let a4 = document.querySelector("#a4");
+
+let a1c = document.querySelector("#a1c");
+let a2c = document.querySelector("#a2c");
+let a3c = document.querySelector("#a3c");
+let a4c = document.querySelector("#a4c");
+
+let a1_content = document.querySelector("#a1-content");
+let a2_content = document.querySelector("#a2-content");
+let a3_content = document.querySelector("#a3-content");
+let a4_content = document.querySelector("#a4-content");
+
+function a1_click() {
+    a1.classList.add("is-active");
+    a2.classList.remove("is-active");
+    a3.classList.remove("is-active");
+    a4.classList.remove("is-active");
+
+    a1c.style.display = "none";
+    a2c.style.display = "block";
+    a3c.style.display = "block";
+    a4c.style.display = "block";
+
+    a1_content.style.display = "block";
+    a2_content.style.display = "none";
+    a3_content.style.display = "none";
+    a4_content.style.display = "none";
+}
+
+function a2_click() {
+    a2.classList.add("is-active");
+    a1.classList.remove("is-active");
+    a3.classList.remove("is-active");
+    a4.classList.remove("is-active");
+    a2c.style.display = "none";
+    a1c.style.display = "block";
+    a3c.style.display = "block";
+    a4c.style.display = "block";
+
+    a2_content.style.display = "block";
+    a1_content.style.display = "none";
+    a3_content.style.display = "none";
+    a4_content.style.display = "none";
+}
+
+function a3_click() {
+    a3.classList.add("is-active");
+    a2.classList.remove("is-active");
+    a1.classList.remove("is-active");
+    a4.classList.remove("is-active");
+    a3c.style.display = "none";
+    a2c.style.display = "block";
+    a1c.style.display = "block";
+    a4c.style.display = "block";
+
+    a3_content.style.display = "block";
+    a1_content.style.display = "none";
+    a2_content.style.display = "none";
+    a4_content.style.display = "none";
+}
+
+function a4_click() {
+    a4.classList.add("is-active");
+    a2.classList.remove("is-active");
+    a3.classList.remove("is-active");
+    a1.classList.remove("is-active");
+    a4c.style.display = "none";
+    a2c.style.display = "block";
+    a3c.style.display = "block";
+    a1c.style.display = "block";
+
+    a4_content.style.display = "block";
+    a1_content.style.display = "none";
+    a2_content.style.display = "none";
+    a3_content.style.display = "none";
+}
